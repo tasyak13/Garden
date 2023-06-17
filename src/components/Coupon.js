f7f2fdb3268cbbc3350aa58483215bdb98521e68
Kirill <kirklio@yandex.ru>
2023-06-17 17:58:35 +0300

    coupon

--- src/components/Coupon.js
+++ src/components/Coupon.js
@@ -1,6 +1,17 @@
+import "./Coupon.css";
+import gnome from "../img/gnome-coupon.png"
+
 function Coupon() {
     return (
-        <section>Coupon</section>
+        <section className="container coupon">
+            <div><img src={gnome}></img></div>
+            <div>
+                <p>5% off</p>  
+                <p>on the first order</p>
+                <input></input>
+                <button>Get a discount</button>
+            </div>
+        </section>
     )
 }
 
